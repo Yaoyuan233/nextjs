fc2456adeb0d4448ce30fdd3ae233bc406c94504
Yao <43917704+Yaoyuan233@users.noreply.github.com>
2021-07-22 11:54:09 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App_Yao</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
